0f31e49a49a86e855bd44213020ed69efe54196d
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-05-03 19:08:04 -0300

    feat: both apps working iinside containers

--- furia-fanzone/src/App.jsx
+++ furia-fanzone/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Comandos from './Comandos';
 
 const botUrl = import.meta.env.VITE_BOT_URL;
-console.log('Bot URL:', botUrl);
+
 export default function App() {
   return (
     <Router>
